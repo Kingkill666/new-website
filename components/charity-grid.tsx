4c7e11561ba072a656e5b2e52273dcf8cf88b41b
Kingkill666 <michael66gray@yahoo.com>
2025-07-04 10:24:27 -0700

    LPE 100 kids fixed and baldy NFT click

--- components/charity-grid.tsx
+++ components/charity-grid.tsx
@@ -45,7 +45,7 @@ const charities = [
   },
   {
     name: "Little Patriots Embraced",
-    stat: "Over 100 Million Kids Served",
+    stat: "Over 100 Thousand Kids Served",
     description: "Supporting military children and families with care packages and programs.",
     logo: "/images/charity-logos/Little-Patriots-Embraced-logo.png",
     link: "https://www.littlepatriotsembraced.org/",
